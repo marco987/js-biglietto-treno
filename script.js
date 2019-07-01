02750da1d8755dc393d9783238cea56a8962935c
marco987 <pacemarco87@gmail.com>
2019-07-01 16:50:47 +0200

    Traccia

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Traccia:
+// Calcolo prezzo del treno: Il programma dovrà
+// chiedere all'utente il numero di chilometri e l'età
+// e dovrà calcolare il prezzo totale del viaggio.
+// Il prezzo del biglietto è definito in base ai km
+// (0.21 €/km) ma c'è uno sconto del 20% per i minorenni
+// e del 40% per gli over 65.
+
+
 // Dichiarazione variabili
 var prezzo, km, eta;
 

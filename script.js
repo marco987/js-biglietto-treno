4c64eb783b2742a7dd5e39915bf096b013edb1e4
marco987 <pacemarco87@gmail.com>
2019-07-01 16:46:34 +0200

    Creazione file e Commenti iniziali

---
+++ script.js
@@ -0,0 +1,13 @@
+// Dichiarazione variabili
+var prezzo, km, eta;
+
+// Richiesta km di viaggio
+
+
+// Richiesta età utente
+
+
+// Prezzo totale, senza sconti (0.21 €/km)
+
+
+// Sconti (-20% minorenni; -40% over-65)
